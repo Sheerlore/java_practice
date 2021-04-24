e1c381406b1fe2237f55d3246441202cc9e37199
uai <noema01@outlook.jp>
2021-04-24 13:21:55 +0000

    lesson16

---
+++ lesson16/Sample1.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class Sample1 extends Frame {
+  public static void main(String[] args) {
+    Sample1 sm = new Sample1();
+  }
+
+  public Sample1() {
+    super("Sample");
+
+    addWindowListener(new SampleWindowListener());
+
+    setSize(250, 200);
+    setVisible(true);
+  }
+
+  class SampleWindowListener extends WindowAdapter {
+    public void windowClosing(WindowEvent e) {
+      System.exit(0);
+    }
+  }
+}
