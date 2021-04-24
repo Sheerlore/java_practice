a3b06f512908f88b74ade7d840852e48d26cd7af
uai <noema01@outlook.jp>
2021-04-24 11:53:54 +0000

    lesson13

---
+++ lesson13/pa/Car.java
@@ -0,0 +1,25 @@
+package pa;
+
+class Car {
+  private int num;
+  private double gas;
+
+  public Car() {
+    this.num = 0;
+    this.gas = 0.0;
+    System.out.println("Create Car!");
+  }
+
+  public void setCar(int n, double g) {
+    this.num = n;
+    this.gas = g;
+    System.out.println("set num = " + this.num);
+    System.out.println("set gas = " + this.gas);
+  }
+
+  public void show() {
+    System.out.println("number is " + this.num);
+    System.out.println("gasling is " + this.gas);
+  }
+
+}
